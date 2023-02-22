d30309cf0c1edc3162f2616a89fa9ae4e1554a49
yangwenxin <yangwenxin45@gmail.com>
2023-02-22 09:47:17 +0800

    集成Element Plus和Pinia

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "HomePage",
+      component: () => import("@/views/control/index.vue"),
+    },
+  ],
+});
+
+export default router;
